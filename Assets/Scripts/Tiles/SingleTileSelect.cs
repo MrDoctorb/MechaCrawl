cb2ba96482b45bee01fa1e9045376a9086c0a1a5
MrDoctorb <zane@indybrowns.org>
2022-04-25 23:06:43 -0500

    Sprites

--- Assets/Scripts/Tiles/SingleTileSelect.cs
+++ Assets/Scripts/Tiles/SingleTileSelect.cs
@@ -6,12 +6,35 @@ public class SingleTileSelect : MonoBehaviour
 {
     public ActionLogic logic;
 
+    public Color color;
+    SpriteRenderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        color.a = .5f;
+        rend.color = color;
+    }
+
     private void OnMouseDown()
     {
         logic.SelectTarget(transform.position);
-        foreach(SingleTileSelect tileSelect in FindObjectsOfType<SingleTileSelect>())
+        foreach (SingleTileSelect tileSelect in FindObjectsOfType<SingleTileSelect>())
         {
             Destroy(tileSelect.gameObject);
         }
     }
+    private void OnMouseEnter()
+    {
+        rend.color = color + new Color(.25f, .25f, .25f, 0);
+
+    }
+
+    private void OnMouseExit()
+    {
+        rend.color = color;
+
+    }
+
+
 }
